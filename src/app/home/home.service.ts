9bfd23f475e8c4e8c018adde6943f3d660c3ee56
Wutthinan <w.wutthinan@gmail.com>
2017-04-27 11:00:24 +0700

    Add git hub page environment

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class HomeService {
 
+  env = environment;
+
   constructor(private http: Http) { }
 
   getItem() {
-    return this.http.get('/assets/mock/data.json');
+    return this.http.get(this.env.assetsPath + '/assets/mock/data.json');
   }
 
 }
